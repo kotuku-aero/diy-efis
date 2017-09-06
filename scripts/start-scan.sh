3eb80d66f41c8882be9b4298fa6f7acefbda4d12
Peter Nunn <peter.nunn@vistic.net>
2017-09-06 14:30:28 +1200

    Removed projects from pre-2017 release

--- scripts/start-scan.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-setserial /dev/$1 baud_base 961200 divisor 15 spd_cust
-slcan_attach -o -s4 /dev/$1
-slcand -o -c -f -S38400 -s4 /dev/$1 $2
-ifconfig $2 up
-
-
-
-
--- scripts/start-vcan.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sudo modprobe can
-sudo modprobe can-raw
-sudo modprobe vcan
-sudo ip link add dev can0 type vcan
-sudo ifconfig can0 up
-
