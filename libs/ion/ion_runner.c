421e382c0d09ad9bceb4e6ffde2d0879164e7efc
Peter Nunn <peter.nunn@kotuku.aero>
2018-02-11 20:20:35 +1300

    Work on embedded stack size

--- libs/ion/ion_runner.c
+++ libs/ion/ion_runner.c
@@ -382,8 +382,7 @@ result_t ion_create_worker(memid_t home,
 
   // create the worker thread
   if (create_worker &&
-    failed(result = task_create(name, DEFAULT_STACK_SIZE, ion_worker,
-      *ion, priority, &ion_ctx->worker)))
+    failed(result = task_create(name, 1024, ion_worker, *ion, priority, &ion_ctx->worker)))
     {
     // release the context..
     ion_close(ion_ctx);
