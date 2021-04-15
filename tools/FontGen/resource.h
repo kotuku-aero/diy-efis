9e8a9cb3768aca5e94c6f4d0d136ae3323c2267d
Peter Nunn <peter.nunn@kotuku.aero>
2021-04-16 09:11:46 +1200

    Restore font generator that converts ttf fonts to CanFly fixed bitmap fonts

---
+++ tools/FontGen/resource.h
@@ -0,0 +1,41 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by FontGen.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_FONTGEN_DIALOG              102
+#define IDR_MAINFRAME                   128
+#define IDC_FONT                        1000
+#define IDC_FONT_FACE                   1000
+#define IDC_FONTSIZE                    1001
+#define IDC_FILENAME                    1002
+#define IDC_BROWSE                      1003
+#define IDC_SELECT                      1004
+#define IDC_ROTATED                     1005
+#define IDC_CHARACTER_SET               1005
+#define IDC_RADIUS                      1006
+#define IDC_FONT2                       1006
+#define IDC_FONT_NAME                   1006
+#define IDC_START_ANGLE                 1007
+#define IDC_GLYPHS                      1007
+#define IDC_FONTSIZES                   1007
+#define IDC_STEP_ANGLE                  1008
+#define IDC_C_ARRAY                     1009
+#define IDC_BASE64                      1010
+#define IDC_BINARY                      1011
+#define IDC_ADD                         1012
+#define IDC_REMOVE                      1013
+#define IDC_DEFAULT_SET                 1015
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1016
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
