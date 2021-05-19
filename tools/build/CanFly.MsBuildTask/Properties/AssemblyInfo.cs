23b8bb06e485f4096254c8c1e93530afb78d6a8c
Peter Nunn <peter.nunn@kotuku.aero>
2021-05-20 11:38:04 +1200

    MSBuild

---
+++ tools/build/CanFly.MsBuildTask/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CanFly Metadata processor MSBuild Task library")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Kotuku Aerospace")]
+[assembly: AssemblyProduct("CanFly Metadata processor MSBuild Task library")]
+[assembly: AssemblyCopyright("Copyright ©  2021 Kotuku Aerospace")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
