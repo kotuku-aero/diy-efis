88cce09ac651e97a713ec67dd1555c3bacc4fe22
Peter Nunn <peter.nunn@kotuku.aero>
2026-07-07 20:47:56 +1200

    MFD5 layout

---
+++ apps/mfd5/main.c
@@ -0,0 +1,26 @@
+#include "../../src/mfdlib/mfd.h"
+
+#include "mfd5.h"
+
+static result_t my_wndproc(handle_t hndl, const canmsg_t* msg, void* wnddata)
+  {
+  // add event handlers here
+
+  return mfd_wndproc(hndl, msg, wnddata);
+  }
+
+#ifndef MIPS32_BUILD
+// debug arguments:
+// -c 4096 -x 320 -y 240 canfly.cdb
+int main(int argc, char** argv)
+  {
+  if (failed(canfly_init(argc, argv)))
+    return -1;
+#else
+int main()
+  {
+#endif
+
+  return run_proton(0, create_layout, create_mfd5, my_wndproc);
+  }
+
